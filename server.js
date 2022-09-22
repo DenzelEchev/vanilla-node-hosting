4cf541e7d2c0e6e3736b13ead544d63fcfdb737b
Denzel M. Echevarria <denzel.echevarria@gmail.com>
2022-09-21 20:13:54 -0400

    added express

--- server.js
+++ server.js
@@ -1,78 +1,51 @@
-const http = require('http');
-const fs = require('fs')
-const url = require('url');
-const querystring = require('querystring');
-const figlet = require('figlet')
+const express = require('express')
+const app = express()
+const PORT = 8000
+const pokeInfo = {
+    "bulbasaur": {
+      "name": "Bulbasaur",
+      "debutGame": "Red, Blue, Green",
+      "pokeDexNumber": 1,
+    },
+    "charmander": {
+      "name": "Charmander",
+      "debutGame": "Red, Blue, Green",
+      "pokeDexNumber": 4,
+    },
+    "squirtle": {
+    "name": "Squirtle",
+    "debutGame": "Red, Blue, Green",
+    "pokeDexNumber": 7,
+    },
+    "unknown": {
+    "name": "unknown",
+    "debutGame": "unknown",
+    "pokeDexNumber": "???",
+    }
+}
 
-const server = http.createServer((req, res) => {
-  const page = url.parse(req.url).pathname;
-  const params = querystring.parse(url.parse(req.url).query);
-  console.log(page);
-  if (page == '/') {
-    fs.readFile('index.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-  }
-  else if (page == '/secondpage') {
-    fs.readFile('secondpage.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-  }
-  else if (page == '/thirdpage') {
-    fs.readFile('thirdpage.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-  }
-  else if (page == '/api') {
-    if('student' in params){
-      if(params['student']== 'denzel'){
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        const objToJson = {
-          name: "Denzel",
-          status: "Weeb",
-          currentOccupation: "Web Dev"
-        }
-        res.end(JSON.stringify(objToJson));
-      }//student = leon
-      else if(params['student'] != 'denzel'){
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        const objToJson = {
-          name: "To Be Determined",
-          status: "In Progress",
-          currentOccupation: "Searching"
-        }
-        res.end(JSON.stringify(objToJson));
-      }//student != leon
-    }//student if
-  }//else if
-  else if (page == '/css/style.css'){
-    fs.readFile('css/style.css', function(err, data) {
-      res.write(data);
-      res.end();
-    });
-  }else if (page == '/js/main.js'){
-    fs.readFile('js/main.js', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/javascript'});
-      res.write(data);
-      res.end();
-    });
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/secondpage', (req, res) => {
+  res.sendFile(__dirname + '/secondpage.html')
+})
+
+app.get('/thirdpage', (req, res) => {
+  res.sendFile(__dirname + '/thirdpage.html')
+})
+
+app.get('/api/:name', (req,res)=>{
+  const pokeName = req.params.name.toLowerCase()
+  if(pokeInfo[pokeName]){
+    res.json(pokeInfo[pokeName])
   }else{
-    figlet('404!!', function(err, data) {
-      if (err) {
-          console.log('Something went wrong...');
-          console.dir(err);
-          return;
-      }
-      res.write(data);
-      res.end();
-    });
+    res.json(pokeInfo["unknown"])
   }
-});
+  
+})
 
-server.listen(8000);
+app.listen(PORT, () =>{
+  console.log(`The server is running on port ${PORT}`)
+})
